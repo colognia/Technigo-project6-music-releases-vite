3f66fbf886e0736b8d242ad7385027ef07686a2d
colognia <frauke.schmidt@protonmail.com>
2023-09-20 11:48:43 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import data from "./data.json";
+import albumdata from "./data.json";
+import { Album } from "./components/Album";
 
-console.log(data);
+console.log(albumdata);
+// returns albums, which is first and only key in objec
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  // albums.items.artists.name
+  return <Album name="Otto" />
 };
+
+
---
+++ src/components/Album.jsx
@@ -0,0 +1,7 @@
+import { App } from "../App"
+
+export const Album = (props) => {
+    return (
+        <p>Künstler: {props.name}</p>
+    )
+}
